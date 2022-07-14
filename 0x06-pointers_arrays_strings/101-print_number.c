cdef32747b2955458f5c45361e4ae6c8cf315581
Hamza <hamhab19@gmail.com>
2022-07-14 11:32:11 -0700

    Numbers have life

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,7 +2,6 @@
 
 /**
  * print_number - prints an integer;
- * @author: muhabeid
  * @n: integer to be printed;
  */
 void print_number(int n)
@@ -10,14 +9,14 @@ void print_number(int n)
 	unsigned int n1;
 
 	if (n < 0)
-    {
-      n1 = -n;
-      _putchar('-');
-    } 
-  else
-    {
-      n1 = n;
-    }
+	{
+		n1 = -n;
+		_putchar('-');
+	}
+	else
+	{
+		n1 = n;
+	}
 
 	if (n1 / 10)
 		print_number(n1 / 10);
